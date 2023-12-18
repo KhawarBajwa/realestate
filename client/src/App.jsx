d288dd346c197fac00cb1c55fd48fa2439c0fa44
khawarbajwa <khawarasasian@gmail.com>
2023-12-18 10:11:34 +0500

    make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from "./pages/SignIn";
 import SingnUp from "./pages/SingnUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -16,7 +17,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SingnUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute/>} >
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import {FaSearch} from "react-icons/fa"
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector(state => state.user)
   return (
     <header className="bg-slate-800 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -24,8 +26,14 @@ export default function Header() {
             <Link to={"/about"}>
             <li className="hidden sm:inline">About</li>
             </Link>
-            <Link to={"sign-in"}>
-            <li className="hidden sm:inline">Sign in</li>
+            <Link to={"profile"}>
+              { currentUser ? (
+                <img className="rounded-full h-7 w-7 object-cover" src={ currentUser.avatar } alt="profile" />
+        
+              ):(
+                <li className="hidden sm:inline">Sign in</li>
+              )}
+            
             </Link>
         </ul>
       </div>
